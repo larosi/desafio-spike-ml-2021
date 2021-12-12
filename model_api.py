a97e7b3c30dc7a50dc5b9310c593273de5005f57
larosi <luis@odd.co>
2021-12-12 01:56:40 -0300

    feat: send post request with date as query or tree consecutive data rows as body

--- model_api.py
+++ model_api.py
@@ -8,21 +8,53 @@ Created on Fri Dec  3 17:19:31 2021
 import flask as flk
 import json
 import src.model.predictor as model_predictor
+from src.data.dataset_cleasing import compute_std_mean_shift
 import pandas as pd
 
 app = flk.Flask(__name__)
 
 MODEL_PATH = 'models/model.pkl'
+MERGED_DF_PATH = 'data/processed/merged.csv'
+
+df = pd.read_csv(MERGED_DF_PATH)
+df = model_predictor.drop_leche(df)
 my_model = model_predictor.load_sklearn_model(MODEL_PATH)
+
+def parse_date(body):
+    date_str = body['date']
+    dia, mes, ano = date_str.split('-')
+    return int(mes), int(ano)
 
 @app.route('/')
 def hello():
     return "Hola Spike"
 
-@app.route('/model_api', methods=['POST'])
-def model_api():
+@app.route('/predict_by_date', methods=['POST'])
+def predict_by_date():
     body = flk.request.get_json(force=True)
-    X = pd.DataFrame(data=body, index=[0])
+    
+    # parse date
+    mes, ano = parse_date(body)
+    
+    mask = (df.mes==mes) & (df.ano==ano)
+    idx = mask.values.argmax()
+    
+    if (idx-2 >=0) and (idx <=df.shape[0]-1):
+        df_slice = df.loc[idx-2:idx].reset_index(drop=True)
+        X = compute_std_mean_shift(df_slice)
+        prediction = my_model.predict(X)
+    else:
+        prediction = [None]
+
+    data = {'precio_leche': '{}'.format(str(prediction[0]))}
+    
+    return json.dumps(data)
+
+@app.route('/predict_by_rows', methods=['POST'])
+def predict_by_rows():
+    body = flk.request.get_json(force=True)
+    X = pd.DataFrame(data=body)
+    X = compute_std_mean_shift(X)
     prediction = my_model.predict(X)
 
     data = {'precio_leche': '{}'.format(str(prediction[0]))}
--- post_request_sample.py
+++ post_request_sample.py
@@ -8,28 +8,39 @@ Created on Mon Dec  6 22:36:12 2021
 import pandas as pd
 import json
 import requests
+import random
+from datetime import datetime
 
+def do_post(body_dict, ip='127.0.0.1', port=8889, route='predict_by_date'):
+    body_json = json.dumps(body_dict)
+    localhost_url = f'http://{ip}:{port}/{route}'
+    r = requests.post(localhost_url, data = body_json)
+    return r
 
-data_path = r'data/processed/precio_leche.csv'
+data_path = r'data/processed/merged.csv'
 
 df = pd.read_csv(data_path)
 df = df.drop(['Precio_leche'], axis = 1)
-random_sample = df.sample(n=1)
 
+random_idx = random.randint(3, df.shape[0]-1)
+random_slice = df.loc[random_idx-2:random_idx].reset_index(drop=True)
 
-body_dict = {}
-for i in range(0, random_sample.shape[1]):
-    key = random_sample.columns[i]
-    val = random_sample.values[0][i]
-    body_dict[key] = val
+random_date = datetime(year=random_slice.ano.values[-1], month=random_slice.mes.values[-1], day=1)
+random_date_str = random_date.strftime("%d-%m-%Y")
 
-body_json = json.dumps(body_dict)
+""" Post request with date as Query """
 
-route = 'model_api'
-port = 8889
-localhost_url = f'http://127.0.0.1:{port}/{route}'
-header = {'Contect-type': 'application/json'}
-r = requests.post(localhost_url, data = body_json)
-
+body_dict = {'date' : random_date_str}
+r = do_post(body_dict, ip='127.0.0.1', port=8889, route='predict_by_date')
 print(r)
 print(r.text)
+
+""" Post request with tree consecutive rows as Query """
+
+random_slice.drop(['ano','mes'], axis = 1, inplace=True)
+body_dict = random_slice.to_dict()
+
+r = do_post(body_dict, ip='127.0.0.1', port=8889, route='predict_by_rows')
+print(r)
+print(r.text)
+
